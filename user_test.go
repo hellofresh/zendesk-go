53a41e0e2885455941adf1ea660d5aa3c33fece5
Felipe Pieretti Umpierre <fum@hellofresh.com>
2016-04-01 18:36:40 +0200

    Simple test

--- user_test.go
+++ user_test.go
@@ -1,7 +1,15 @@
-package test
+package zendesk
 
-import "testing"
+import (
+	"testing"
+)
 
-func getOneUser(t *testing.T) {
+func TestGetUser(t *testing.T) {
+	client, _ := FromEnv();
 
+	user, _ := client.Users().GetUser(211722549);
+
+	if user.Id != 211722549 {
+		t.Fatal("ERROR")
+	}
 }
