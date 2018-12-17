27a86b919729674860989c9fd1e4e1428c185a1c
Zach Dunton <zach.dunton@smartfrog.com>
2018-12-17 16:56:03 +0100

    Add score to query string when satisfaction ratings are being used

--- satisfaction_ratings.go
+++ satisfaction_ratings.go
@@ -12,8 +12,18 @@ type SatisfactionRatingApiHandler struct {
 }
 
 func (s SatisfactionRatingApiHandler) SatisfactionRatings(score string, startDate *time.Time, stopDate *time.Time) (SatisfactionRatings, error) {
+	var urlString string
+
+	if len(score) > 0) {
+		formatString =  "/satisfaction_ratings.json?start_time=%d&end_time=%d&score=%s"
+		urlString :=		fmt.Sprintf(formatString, startDate.Unix(), stopDate.Unix(), score),
+	} else {
+		formatString:= "/satisfaction_ratings.json?start_time=%d&end_time=%d"
+		urlString :=		fmt.Sprintf(formatString, startDate.Unix(), stopDate.Unix()),
+	}
+	
 	response, err := s.client.get(
-		fmt.Sprintf("/satisfaction_ratings.json?start_time=%d&end_time=%d", startDate.Unix(), stopDate.Unix()),
+		urlString
 		nil,
 	)
 
